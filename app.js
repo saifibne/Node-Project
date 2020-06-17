9b7cbba8b8fe242f6285cf8e6a862fc599e732d8
ibne saif <51085968+saifibne@users.noreply.github.com>
2020-06-18 01:58:10 +0530

    18/06/2020

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
 const http = require('http')
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Node-Server</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write('</html>')
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const messageBody = Buffer.concat(body).toString();
-            const message = messageBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Node-Server</title></head>')
-    res.write('<body><h1>welcome to node js</h1></body>')
-    res.write('</html>')
-    res.end();
-})
+const routes = require('./routes')
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
