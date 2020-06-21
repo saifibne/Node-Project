b44f5f05c5a60e822da3d11286937be4121ed24c
ibne saif <51085968+saifibne@users.noreply.github.com>
2020-06-22 01:37:03 +0530

    22/06/2020

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status("404").sendFile(path.join(__dirname, "views", "404.html"));
+  res.status("404").render("404", { title: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const adminData = require("./admin");
 
+const products = adminData.products;
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.render("shop", { prods: products, title: "Shop", path: "/" });
 });
 
 module.exports = router;
