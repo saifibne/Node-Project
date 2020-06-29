51fc88f69db19dddad8607cbafb9cb0cfa2995b6
ibne saif <51085968+saifibne@users.noreply.github.com>
2020-06-29 21:52:08 +0530

    29/06/2020

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 const errorController = require("./controllers/error");
 const User = require("./models/users");
 
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 });
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
