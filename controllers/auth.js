62a9a8b0ccdbf49ff513e5a113843ba523132da8
ibne saif <51085968+saifibne@users.noreply.github.com>
2020-07-02 02:53:51 +0530

    2/07/20

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,26 @@
 const bCrypt = require("bcryptjs");
+const crypto = require("crypto");
+const nodeMailer = require("nodemailer");
+const sendGridTransporter = require("nodemailer-sendgrid-transport");
 const User = require("../models/users");
 
+const transporter = nodeMailer.createTransport(
+  sendGridTransporter({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 exports.getLogin = (req, res, next) => {
   const message = req.flash("error")[0];
+  const successMessage = req.flash("success")[0];
   console.log(message);
   res.render("auth/login", {
     path: "/login",
     title: "Login",
     errorMessage: message,
+    successMessage: successMessage,
   });
 };
 
@@ -72,6 +85,91 @@ exports.postSignUp = (req, res, next) => {
           return user.save();
         })
         .then((user) => {
+          res.redirect("/login");
+          return transporter
+            .sendMail({
+              to: email,
+              from: "ibnesaif3g@gmail.com",
+              subject: "Account Created",
+              html: "<h1>Account created successfully!!</h1>",
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getReset = (req, res, next) => {
+  const message = req.flash("error")[0];
+  res.render("auth/reset", {
+    path: "/reset",
+    title: "reset Password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  const email = req.body.email;
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      req.flash("error", "Wrong email address!");
+      return res.redirect("/reset");
+    }
+    crypto.randomBytes(32, (err, buffer) => {
+      let token;
+      if (!err) {
+        token = buffer.toString("hex");
+        user.resetToken = token;
+        user.tokenExpire = Date.now() + 3600000;
+        user.save();
+        res.redirect("/reset");
+      }
+      transporter.sendMail({
+        to: email,
+        from: "ibnesaif3g@gmail.com",
+        subject: "Password Reset",
+        html: `<p>localhost:3000/update/${token}</p>`,
+      });
+    });
+  });
+};
+
+exports.getResetPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, tokenExpire: { $gt: Date.now() } }).then(
+    (user) => {
+      const userId = user._id.toString();
+      res.render("auth/reset-password", {
+        path: "/reset-password",
+        title: "reset Password",
+        userId: userId,
+      });
+    }
+  );
+};
+
+exports.postResetPassword = (req, res, next) => {
+  const password = req.body.password;
+  const userId = req.body.userId;
+  User.findOne({ _id: userId, tokenExpire: { $gt: Date.now() } })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/reset");
+      }
+      return bCrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          user.password = hashedPassword;
+          user.resetToken = undefined;
+          user.tokenExpire = undefined;
+          user.save();
+        })
+        .then((result) => {
+          req.flash("success", "Password changed successfully.");
           res.redirect("/login");
         });
     })
--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,12 @@ const authController = require("../controllers/auth");
 
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignUp);
+router.get("/reset", authController.getReset);
+router.get("/update/:token", authController.getResetPassword);
 router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.post("/signup", authController.postSignUp);
+router.post("/reset", authController.postReset);
+router.post("/reset-password", authController.postResetPassword);
 
 module.exports = router;
